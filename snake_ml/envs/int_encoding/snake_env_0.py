e1ace3b3cd2e4f0764496f87bba80e9d08fdf1a3
Hannes Piechulek <hannes.piechulek@telekom.de>
2025-08-27 08:10:38 +0200

    added int encoded version

--- snake_ml/envs/int_encoding/snake_env_0.py
+++ snake_ml/envs/int_encoding/snake_env_0.py
@@ -5,9 +5,8 @@ from gymnasium import spaces
 import numpy as np
 
 # CHANGES
-# TODO instead of one-hot encoding this version should learn the embedding itself
+# TODO instead of one-hot encoding this version should use simple int encoding
 # this would greatly reduce the amount if inputs
-# or alternatively just us ints for the different values
 # Action mapping
 ACTION_MAP = {
     0: (-1, 0),   # up
@@ -27,7 +26,7 @@ class SnakeEnv(gym.Env):
         # Observation: 14x14x4 one-hot -> flattened
         self.observation_space = spaces.Box(
             low=0.0, high=1.0,
-            shape=(board_size * board_size * 4,),
+            shape=(board_size * board_size,),
             dtype=np.float32
         )
 
@@ -48,7 +47,6 @@ class SnakeEnv(gym.Env):
         self.food = (2*self.board_size // 3, self.board_size // 2)
 
         self.steps = 0
-        self.steps_food = 0
         self.done = False
         obs = self._get_obs()
         return obs, {}
@@ -57,7 +55,6 @@ class SnakeEnv(gym.Env):
         action = int(action)
 
         self.steps += 1 
-        self.steps_food += 1
         if self.steps > self.board_size * 1.2:
             return self._get_obs(), -15.0, True, False, {}
 
@@ -89,13 +86,13 @@ class SnakeEnv(gym.Env):
         else:
             self.snake.pop()    
 
-
-
-        pos_new, pos_old = self.snake[:2]
-        if math.dist(pos_new, self.food) < math.dist(pos_old, self.food):
-            reward = 0.05  # reward for moving closer
-        else:
-            reward = -0.002 if self.steps_food > len(self.snake) * 1.2 else 0
+        reward = 0
+        if len(self.snake) > 1:
+            pos_new, pos_old = self.snake[:2]
+            if math.dist(pos_new, self.food) < math.dist(pos_old, self.food):
+                reward = 0.05  # reward for moving closer
+            else:
+                reward = -0.002 * 2/len(self.snake) if self.steps > len(self.snake) * 1.2 else 0
         
         obs = self._get_obs()
         return obs, reward, self.done, False, {}
@@ -108,24 +105,24 @@ class SnakeEnv(gym.Env):
         
         step_panelty = -0.002 * 2/len(self.snake)
 
-        reward = step_panelty if self.steps_food > len(self.snake) * 1.2 else 0
+        reward = step_panelty if self.steps > len(self.snake) * 1.2 else 0
 
         return reward
-
-    def _get_obs(self):
-        board = np.zeros((self.board_size, self.board_size, 4), dtype=np.float32)
-        for x, y in self.snake[1:]:
-            board[y, x, 2] = 1.0  # body
-        head_x, head_y = self.snake[0]
-        board[head_y, head_x, 1] = 1.0  # head
-        food_x, food_y = self.food
-        board[food_y, food_x, 3] = 1.0  # food
-        return board.flatten()
 
     def _place_food(self):
         free_cells = [(x, y) for x in range(self.board_size) for y in range(self.board_size) if (x, y) not in self.snake]
         self.food = tuple(self.np_random.choice(free_cells))
-        self.steps_food = 0
+        self.steps = 0
+
+    def _get_obs(self):
+        board = np.zeros((self.board_size, self.board_size), dtype=np.float32)
+        for x, y in self.snake[1:]:
+            board[y, x] = 1.0  # body
+        head_x, head_y = self.snake[0]
+        board[head_y, head_x] = 2.0  # head
+        food_x, food_y = self.food
+        board[food_y, food_x] = 3.0  # food
+        return board.flatten()
 
     def render(self):
         if self.render_mode == "human":
--- snake_ml/register.py
+++ snake_ml/register.py
@@ -1,18 +1,34 @@
 from gymnasium.envs.registration import register
 
+#one_hot encoding
 register(
+    # rewards death food and win
     id="Snake-one-hot-v0",
     entry_point="snake_ml.envs.one_hot.snake_env_0:SnakeEnv",
 )
 register(
+    # truncate after board_size * 1.2 steps
+    # reward distance reduction by 0.05 otherwise -0.002
     id="Snake-one-hot-v1",
     entry_point="snake_ml.envs.one_hot.snake_env_1:SnakeEnv",
 )
 register(
+    # only punish steps after steps_since_food > len(snake)*1.2
+    # TODO consider passing steps as parameter
     id="Snake-one-hot-v2",
     entry_point="snake_ml.envs.one_hot.snake_env_2:SnakeEnv",
 )
 register(
+    # reward scaled by length. Staying alive longer better
+    # scale step panalty
+    # TODO consider passing steps as parameter
     id="Snake-one-hot-v3",
     entry_point="snake_ml.envs.one_hot.snake_env_3:SnakeEnv",
+)
+
+# int encoding
+register(
+    # based on Snake-one-hot-v3
+    id="Snake-int-v0",
+    entry_point="snake_ml.envs.int_encoding.snake_env_0:SnakeEnv",
 )
