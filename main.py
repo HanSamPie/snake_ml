a09427cf325fa6b60f80001d04ac0a1b548655c4
Hannes Piechulek <hannes.piechulek@telekom.de>
2025-08-27 13:35:18 +0200

    merged training scripts

--- main.py
+++ main.py
@@ -6,25 +6,13 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 import torch
 
-if __name__ == "__main__":
-    n_envs = 32
-    n_steps = 512          # rollout per env
-    total_rollout = n_envs * n_steps  # = 16,384
-    batch_size = 1024      # divides 16,384 evenly
-    n_epochs = 10          # you can try 5–8 if speed is critical
-    env = make_vec_env("Snake-one-hot-v0", n_envs=n_envs)
-    policy_kwargs = dict(
-        net_arch=dict(
-            pi=[512, 256, 128],
-            vf=[512, 256, 128]
-        ),
-        activation_fn=torch.nn.ReLU
-    )
 
+def train():
+    env = make_vec_env(env_str, n_envs=n_envs)
     model = PPO(
         "MlpPolicy",
         env,
-        device="cpu",       # your GPU will be used
+        device=device,
         n_steps=n_steps,
         batch_size=batch_size,
         n_epochs=n_epochs,
@@ -42,9 +30,69 @@ if __name__ == "__main__":
     # profiler = cProfile.Profile()
     # profiler.enable()
 
-    model.learn(total_timesteps=10_000)
-    model.save("ppo_snake")
+    model.learn(total_timesteps=timesteps)
+    model.save(model_path)
 
     # profiler.disable()
     # stats = pstats.Stats(profiler)
     # stats.sort_stats("cumtime").print_stats(50)  
+
+
+def load_train():
+    env = make_vec_env(env_str, n_envs=n_envs)
+
+    model = PPO.load(
+        path=model_path,
+        env=env,
+        device=device
+    )
+
+    model.learn(total_timesteps=timesteps)
+    model.save(model_path)
+
+
+def test(max_steps=200, render=True):
+    # Load the trained model
+    model = PPO.load(model_path)
+
+    # Create a single environment
+    env = gym.make(env_str, render_mode="human")
+    
+    obs, info = env.reset()
+    for step in range(max_steps):
+        # Model predicts an action
+        action, _ = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(action)
+        
+        if render:
+            env.render()
+        
+        if terminated or truncated:
+            print(f"Episode ended after {step+1} steps, reward={reward}")
+            break
+
+    env.close()
+
+if __name__ == "__main__":
+    env_str = "Snake-one-hot-v0"
+    model_path = "ppo_snake.zip"
+    device = "cpu"
+    timesteps=10_000
+
+    n_envs = 32
+    n_steps = 512          # rollout per env
+    total_rollout = n_envs * n_steps  # = 16,384
+    batch_size = 1024      # divides 16,384 evenly
+    n_epochs = 10          # you can try 5–8 if speed is critical
+    policy_kwargs = dict(
+        net_arch=dict(
+            pi=[512, 256, 128],
+            vf=[512, 256, 128]
+        ),
+        activation_fn=torch.nn.ReLU
+    )
+
+    
+    train()
+    load_train()
+    test()
