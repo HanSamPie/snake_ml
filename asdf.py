752d2cfdfed2aca19ed20574b4bb90417fdb4fab
Hannes <hanpie0501@gmail.com>
2025-10-07 18:13:30 +0200

    draft kde plot

--- asdf.py
+++ asdf.py
@@ -1,6 +1,10 @@
 import json
 from pathlib import Path
 
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 with open('./aggregated-data.json', 'r') as file:
     data = json.load(file)
 
@@ -21,12 +25,40 @@ for version in data:
     print(last_episode['checkpoint_path'])
     causes = { 'deprecated': 0, 'snake': 0, 'border': 0 }
     scores = []
+
+    df = pd.DataFrame(last_episode["death_details"])
+
+    cause_colors = {
+        'snake': 'green',  # A specific shade of red
+        'border': 'blue',  # A specific shade of blue
+        'deprecated': 'grey'
+    }
+
+
+    plt.figure(figsize=(10, 6))
+
+    # Pass the color dictionary to the 'palette' parameter
+    sns.kdeplot(
+        data=df,
+        x='score',
+        hue='cause',
+        fill=True,
+        palette=cause_colors # This is the key change
+    )
+
+    name = last_episode['checkpoint_path'].split("/")
+    name = name[2]+ "-" + name[3]
+    plt.title(f'Score Distribution by Cause {name}')
+    plt.xlabel('Score')
+    plt.ylabel('Density')
+    plt.savefig(f'seaborn_kde_plot{name}.png')
+    plt.close()
+
     for death in last_episode["death_details"]:
         causes[death["cause"]] += 1
         scores.append(death["score"])
     mean_score = sum(scores)/len(scores)
-    print(causes)
-    print(mean_score)
+    print(causes, mean_score)
     # for episode in version:
     #     print(episode['checkpoint_path'])
     #     causes = { 'deprecated': 0, 'snake': 0, 'border': 0 }
